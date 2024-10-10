11bf7d528eb84bfba75ecc7284dc25240b2203ea
Vallor23 <enosnyabinda2017@gmail.com>
2024-10-10 08:51:50 +0300

    feat(Header): integrate NavBar for navigation links and ThemeToggle component for theme switching

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,21 @@
+import ThemeToggle from "./ThemeToggle";
+import NavBar from './NavBar';
+
 import { FaVideo } from "react-icons/fa";
 // Header section
 const Header = () => {
   return (
-    <div className='flex w-auto gap-2 items-center justify-center p-4 bg-darkCharcoal'>
-        <FaVideo className='bg-brightAmber w-8 h-6'/>
-        <p className='text-2xl text-lightGray uppercase font-bold'>movie mania</p>
-    </div>
+      <div className='flex items-center justify-between p-4  relatve bg-customColor'>
+        <NavBar />
+        <div className="flex-grow flex items-center justify-center">
+          <FaVideo className='fill-brightAmber w-8 h-6 md:w-10 md:h-8'/>
+          <p className='text-2xl lg:text-3xl text-lightGray uppercase font-bold'>movie mania</p>
+        </div>
+        <div className="absolute top-6 right-1 md:top-4 md:right-4">
+          <ThemeToggle />
+        </div>
+      </div>
   )
 }
 
-export default Header
+export default Header;
